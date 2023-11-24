567097e12fd395e5e58cf949b866c536a63e1636
alanbebermartins <abebermartins@gmail.com>
2023-11-24 15:02:59 -0300

    Desafio calculo progresso de nivel do heroi

---
+++ Desafio_03_progesso_nivel.js
@@ -0,0 +1,30 @@
+let posicaoInicial;
+let totalpassos;
+let posicaoFinal1;
+let posicaoFinal2;
+let posicaoFinal3;
+
+
+// function somaPosicao(posicaoInicial, totalpassos){
+//     soma = posicaoInicial + totalpassos 
+//     return soma
+// }
+
+// posicaoFinal1 = somaPosicao(2, 3)
+// posicaoFinal2 = somaPosicao(15, 3)
+// posicaoFinal3 = somaPosicao(10, 6)
+
+// console.log(`Posicao final do heroi: ${posicaoFinal1}`)
+// console.log(`Posicao final do heroi: ${posicaoFinal2}`)
+// console.log(`Posicao final do heroi: ${posicaoFinal3}`)
+
+function somaPosicao(posicaoInicial, totalpassos){
+    soma = posicaoInicial + totalpassos 
+    return soma
+}
+
+somaPosicao(2, 3)
+somaPosicao(15, 3)
+somaPosicao(10, 6)
+
+console.log(`Posicao final do heroi: ${soma}`)
